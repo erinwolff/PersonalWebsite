d602c507d3273f49106e8f6b2e2a6432219cb7f1
Erin Wolff <erin_wolff@outlook.com>
2023-10-01 11:03:52 -0700

    initial commit of Personal Website project

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2309.Unit1.PersonalWebsite
+Erin Wolff <br>
+Personal Website Project for FSA cohort 2309
---
+++ greeting.js
@@ -0,0 +1,4 @@
+const name = ("Erin Wolff")
+const greeting = ("Hi! My name is")
+
+console.log(greeting + " " + name)
